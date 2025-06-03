d3d0cbb71a755165471438e389a575c9c3ba15a7
Girts <girts@gkarcevskis.com>
2025-06-03 08:09:31 +0200

    implement create node and start on command node

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include "libft.h"
+#include <cstdlib>
 #include <stdio.h>
 #include "minishell.h"
 
-void handle_command(t_token *token, t_ast *ast)
+void clean_exit(char *error)
 {
 	//TODO
 }
 
-t_ast create_ast_node(void)
+void handle_command(t_token *token, t_ast *ast)
 {
 	//TODO
+			
+}
+
+t_ast *create_ast_node(t_ast_node_type type)
+{
+	//TODO
+	t_ast *node;
+
+	node = (t_ast)malloc(sizeof(t_ast_node));
+	if (!node)
+		clean_exit("Failed to allocate memore for a node!\n");
+	return (node);
 }
 
 void free_ast(void)
@@ -23,6 +36,7 @@ int main (void)
 {
 	t_ast *ast;
 
+	ast = create_ast_node();
     printf("Generating Test Input 1 (ls -l /tmp):\n");
     t_token *tokens1 = get_test_input_1_tokens();
    	// print_token_list(tokens1);
