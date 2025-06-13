bf1b7f0bd6d49a8cd0002fc413ca36f885dd1eb5
Girts <girts@gkarcevskis.com>
2025-06-13 16:08:23 +0200

    Remove main and replace with print helpers, for merge

--- print_helpers.c
+++ print_helpers.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+// /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -35,20 +35,20 @@ void	ft_print_token_list(t_token *token)
 	}
 }
 
-int main(int argc, char **argv, char **envp)
-{
-	t_env	*env;
-	t_token	*token_list;
-
-	(void)argc;
-	(void)argv;
-	env = ft_init_env(envp);
-	ft_print_env_list(env);
-	ft_free_env(env);
-	// (void)envp;
-	token_list = ft_new_token_node();
-	ft_populate_token_list(token_list, "ls -l|wc -l");
-	ft_print_token_list(token_list);
-	ft_free_token_list(token_list);
-	return (0);
-}
+// int main(int argc, char **argv, char **envp)
+// {
+// 	t_env	*env;
+// 	t_token	*token_list;
+//
+// 	(void)argc;
+// 	(void)argv;
+// 	env = ft_init_env(envp);
+// 	ft_print_env_list(env);
+// 	ft_free_env(env);
+// 	// (void)envp;
+// 	token_list = ft_new_token_node();
+// 	ft_populate_token_list(token_list, "ls -l|wc -l");
+// 	ft_print_token_list(token_list);
+// 	ft_free_token_list(token_list);
+// 	return (0);
+// }
