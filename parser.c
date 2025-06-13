46a06592c4caa0822f68f559bc56ec1f78b191d0
Girts <girts@gkarcevskis.com>
2025-06-13 15:57:19 +0200

    Remove main and rename as parser.c for merging

--- parser.c
+++ parser.c
@@ -27,15 +27,15 @@ t_ast	*parser(t_token *tokens)
 	return (root);
 }
 
-int	main(void)
-{
-	t_ast	*root;
-	t_token	*tokens1;
-
-	tokens1 = get_test_input_11_tokens();
-	root = parser(tokens1);
-	print_ast(root);
-	free_token_list(tokens1);
-	cleanup(root);
-	return (1);
-}
+// int	main(void)
+// {
+// 	t_ast	*root;
+// 	t_token	*tokens1;
+//
+// 	tokens1 = get_test_input_11_tokens();
+// 	root = parser(tokens1);
+// 	print_ast(root);
+// 	free_token_list(tokens1);
+// 	cleanup(root);
+// 	return (1);
+// }
