b9b157128e8eb3a2c7ad0c5ccc04f674978f8dfe
Girts <girts@gkarcevskis.com>
2025-06-19 22:18:06 +0200

    Finished with norm

--- execute_ast.c
+++ execute_ast.c
@@ -5,67 +5,62 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void execute_simple_command(t_ast *node)
+//check if node has any info about redirs
+//if yes then open the dest file keeping fd.keep in mind the permissions needed
+//use dup2 function to update the stdin or stdout based on the redir type
+//close the fd of the dest file that was opened before
+void	execute_simple_command(t_ast *node)
 {
-	char **args;
-	char *command;
+	char	**args;
+	char	*command;
 
 	args = node->data.command_node.args;
 	command = ft_strjoin("/bin/", node->data.command_node.value);
-	//check if node has any info about redirs
-	//if yes then open the dest file keeping fd.keep in mind the permissions needed
-	//use dup2 function to update the stdin or stdout based on the redir type
-	//close the fd of the dest file that was opened before
 	execve(command, args, NULL);
 	free(command);
 	command = NULL;
 	exit(1);
 }
 
-void execute_ast(t_ast *root)
+void	execute_pipe(int pipe_fd[2], t_ast *root)
 {
-	int child;
-	int left_child;
-	// int right_child;
-	int status;
-	int pipe_fd[2];
+	int	left_child;
+
+	if (pipe(pipe_fd) == -1)
+	{
+		perror("Error creating pipe!\n");
+		exit(1);
+	}
+	left_child = fork();
+	if (left_child == 0)
+	{
+		close(pipe_fd[0]);
+		dup2(pipe_fd[1], 1);
+		close(pipe_fd[1]);
+		if (root->left->type == PIPE_NODE)
+			execute_ast(root->left);
+		else
+			execute_simple_command(root->left);
+	}
+	close(pipe_fd[1]);
+	dup2(pipe_fd[0], 0);
+	close(pipe_fd[0]);
+	if (root->right->type == PIPE_NODE)
+		execute_ast(root->right);
+	else
+		execute_simple_command(root->right);
+}
+
+void	execute_ast(t_ast *root)
+{
+	int	child;
+	int	status;
+	int	pipe_fd[2];
 
 	if (!root)
 		return ;
 	if (root->type == PIPE_NODE)
-	{
-		if (pipe(pipe_fd) == - 1)
-		{
-			perror("Error creating pipe!\n");
-			exit(1); //should be clean exit
-		}
-		left_child = fork();
-		if (left_child == 0)
-		{
-			close(pipe_fd[0]);
-			dup2(pipe_fd[1], 1);
-			close(pipe_fd[1]);
-			if (root->left->type == PIPE_NODE)
-				execute_ast(root->left);
-			else
-			 	execute_simple_command(root->left);
-		}
-		// right_child = fork();
-		// if (right_child == 0)
-		// {
-		close(pipe_fd[1]);
-		dup2(pipe_fd[0], 0); 
-		close(pipe_fd[0]);
-		if (root->right->type == PIPE_NODE)
-			execute_ast(root->right);
-		else
-			execute_simple_command(root->right);
-	// }
-		// close(pipe_fd[0]);
-		// close(pipe_fd[1]);
-		// waitpid(left_child, &status, 0);
-		// waitpid(right_child, &status, 0);
-	}
+		execute_pipe(pipe_fd, root);
 	else if (root->type == COMMAND_NODE)
 	{
 		child = fork();
@@ -74,9 +69,7 @@ void execute_ast(t_ast *root)
 		else if (child > 0)
 			waitpid(child, &status, 0);
 		else
-		 	perror("Error forking a child!\n");
+			perror("Error forking a child!\n");
 		rl_on_new_line();
 	}
 }
-
-
--- main.c
+++ main.c
@@ -3,15 +3,29 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+void	execution(t_ast *root)
+{
+	int	child;
+	int	status;
+
+	child = fork();
+	if (child == -1)
+		perror("Failed to fork a child!\n");
+	else if (child == 0)
+	{
+		execute_ast(root);
+		exit(0);
+	}
+	else
+		waitpid(child, &status, 0);
+}
+
 int	main(void)
 {
 	t_ast	*root;
 	t_token	*tokens;
 	char	*input;
-	int		child;
-	int		status;
 
-	status = 0;
 	while (1)
 	{
 		input = readline("$> ");
@@ -26,16 +40,7 @@ int	main(void)
 		ft_populate_token_list(tokens, input);
 		root = parser(tokens);
 		print_ast(root);
-		child = fork();
-		if (child == -1)
-			perror("Failed to fork a child!\n");
-		else if (child == 0)
-		{
-			execute_ast(root);	
-			exit(0);
-		}
-		else
-			waitpid(child, &status, 0);	
+		execution(root);
 		rl_on_new_line();
 		ft_free_token_list(tokens);
 		cleanup(root);
